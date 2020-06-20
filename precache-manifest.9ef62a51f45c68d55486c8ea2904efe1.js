d7b5e1fd7094af92fc0b5966b5b93b9a61d3de70
Electron-2002 <satej.bidvai@gmail.com>
2020-06-21 04:22:44 +0530

    Updates

---
+++ precache-manifest.9ef62a51f45c68d55486c8ea2904efe1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242c4bcf12972285beacf4df87b88c01",
+    "url": "/react-blog-app.git/index.html"
+  },
+  {
+    "revision": "b6938f97c65986d16e3e",
+    "url": "/react-blog-app.git/static/css/main.5391fe9d.chunk.css"
+  },
+  {
+    "revision": "4943bd8c6fccfa26f3db",
+    "url": "/react-blog-app.git/static/js/2.8b12436a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog-app.git/static/js/2.8b12436a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6938f97c65986d16e3e",
+    "url": "/react-blog-app.git/static/js/main.739d9792.chunk.js"
+  },
+  {
+    "revision": "e79528a76e4c83d64c7b",
+    "url": "/react-blog-app.git/static/js/runtime-main.00ea99f5.js"
+  },
+  {
+    "revision": "80961a00ad954f1df9a3356d8e386900",
+    "url": "/react-blog-app.git/static/media/affection-baby-baby-girl-beautiful-377058.80961a00.jpg"
+  },
+  {
+    "revision": "4218ad9257e6d8b09e88ad06c8308e04",
+    "url": "/react-blog-app.git/static/media/beautiful-&-simple.4218ad92.jpg"
+  },
+  {
+    "revision": "a67df1389674b05ae6d1b50ad59a30b1",
+    "url": "/react-blog-app.git/static/media/cestovat-chladny-dno-jednoduchost-2868847.a67df138.jpg"
+  },
+  {
+    "revision": "45c60c8a7cce7c53f5988fcdb2b71656",
+    "url": "/react-blog-app.git/static/media/fitness-blog-post.45c60c8a.jpg"
+  },
+  {
+    "revision": "13ad316bf606c371e9f32ad600e9f949",
+    "url": "/react-blog-app.git/static/media/memories-from.13ad316b.jpg"
+  }
+]);
